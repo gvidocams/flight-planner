f4f6ed3cbf36e3deda29e68631682e244e07d50d
Gvido <gaca6817@gmail.com>
2022-09-23 15:12:09 +0300

    Finished test 01 02 03

---
+++ Controllers/AdminApiController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("admin-api")]
+    [ApiController, Authorize]
+    public class AdminApiController : ControllerBase
+    {
+        [Route("flights/{id}")]
+        [HttpGet]
+        public IActionResult GetFlight(int id)
+        {
+            var flight = FlightStorage.GetFlight(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(flight);
+        }
+
+        [Route("flights")]
+        [HttpPut]
+        public IActionResult PutFlight(Flight flight)
+        {
+            if (flight.From == null ||
+                flight.To == null ||
+                String.IsNullOrEmpty(flight.From.Country) ||
+                String.IsNullOrEmpty(flight.From.City) ||
+                String.IsNullOrEmpty(flight.From.AirportCode) ||
+                String.IsNullOrEmpty(flight.To.Country) ||
+                String.IsNullOrEmpty(flight.To.City) ||
+                String.IsNullOrEmpty(flight.To.AirportCode) ||
+                String.IsNullOrEmpty(flight.Carrier) ||
+                String.IsNullOrEmpty(flight.DepartureTime) ||
+                String.IsNullOrEmpty(flight.ArrivalTime))
+            {
+                return BadRequest();
+            }
+
+            if (flight.From.Country.ToLower().Trim() == flight.To.Country.ToLower().Trim() &&
+                flight.From.City.ToLower().Trim() == flight.To.City.ToLower().Trim() &&
+                flight.From.AirportCode.ToLower().Trim() == flight.To.AirportCode.ToLower().Trim())
+            {
+                return BadRequest();
+            }
+
+            var departure = DateTime.Parse(flight.DepartureTime);
+            var arrival = DateTime.Parse(flight.ArrivalTime);
+
+            if(departure >= arrival)
+            {
+                return BadRequest();
+            }
+
+            if(FlightStorage.IsDuplicate(flight))
+            {
+                return Conflict();
+            }
+
+            flight = FlightStorage.AddFlight(flight);
+            return Created("", flight);
+        }
+    }
+}
+
---
+++ FlightStorage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlightPlanner
+{
+    public static class FlightStorage
+    {
+        private static List<Flight> _flights = new List<Flight>();
+        private static int _id = 0;
+
+        public static Flight AddFlight(Flight flight)
+        {
+            flight.Id = ++_id;
+            _flights.Add(flight);
+            return flight;
+        }
+
+        public static Flight GetFlight(int id)
+        {
+            return _flights.FirstOrDefault(f => f.Id == id);
+        }
+
+        public static void Clear()
+        {
+            _flights.Clear();
+            _id = 0;
+        }
+
+        public static bool IsDuplicate(Flight flight)
+        {
+            foreach (Flight f in _flights)
+            {
+                if(f.From.Country == flight.From.Country &&
+                   f.From.City == flight.From.City &&
+                   f.From.AirportCode == flight.From.AirportCode &&
+                   f.To.Country == flight.To.Country &&
+                   f.To.City == flight.To.City &&
+                   f.To.AirportCode == flight.To.AirportCode &&
+                   f.Carrier == flight.Carrier &&
+                   f.DepartureTime == flight.DepartureTime &&
+                   f.ArrivalTime == flight.ArrivalTime)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
